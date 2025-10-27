1cb3c4deb94c22e7e47bb0847d86066f62108936
kali <kali@kali>
2025-10-27 09:07:53 +0100

    add the authentification method and HTTP payload can be modified

--- bruteforce.py
+++ bruteforce.py
@@ -1,37 +1,47 @@
 import requests
 import argparse
 from pathlib import Path
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qsl, urlencode
 import sys
 
 ####################
 # global variables #
 ####################
 found = False
+mode_list = ["http-post-json"]
 
 #get bad login response to make the filtering a good response
-def bad_request(url):
-    data = {
-        "email": "ImpossibleEmailqsdazer1234321",
-        "password": "ImpossiblePassowrdqsdazer1234321"
-    }
+def httppostjson_bad_request(url,data):
+
+    filled = data.replace("^USER^", "input1").replace("^PASS^", "input2")
+    data = dict(parse_qsl(filled))
+
     headers = {
         "Content-Type": "application/json"
     }
+
     response = requests.post(url, json=data, headers=headers)
     bad_request = {}
     bad_request["status"] = response.status_code
     bad_request["text"] = response.text
     print("Failed HTTP status : "+str(response.status_code))
     print("Failed HTTP body : "+response.text)
+    print("Failed HTTP request : ")
+    print("---")
+    req = response.request
+    print(f"{req.method} {req.url}")
+    for i in dict(req.headers):
+        print(i+"="+req.headers[i])
+    print(urlencode(data))
+    print("---")
     return bad_request
 
-def send(url,username,password,bad_request_data):
+def httppostjson(url,username,password,bad_request_data,data):
     global found 
-    data = {
-        "email": username,
-        "password": password
-    }
+
+    filled = data.replace("^USER^", username).replace("^PASS^", password)
+    data = dict(parse_qsl(filled))
+    
     headers = {
         "Content-Type": "application/json"
     }
@@ -53,6 +63,9 @@ def load_lines(path):
         return [line.strip() for line in f if line.strip() and not line.strip().startswith("#")]
 
 def main(argv=None):
+    global found 
+    global mode_list
+
     if argv is None:
         argv = sys.argv[1:]
 
@@ -63,6 +76,10 @@ def main(argv=None):
 
     parser.add_argument("-h", "--host", dest="host", required=True,
                         help="Target host URL (e.g. http://example.com)")
+    parser.add_argument("-m", "--mode", dest="mode", required=True,
+                        help="This is the authentification mode")
+    parser.add_argument("-d", "--data", dest="data", required=True,
+                        help="This shows how the data is set")
     parser.add_argument("-u", "--user-file", dest="user_file", required=True,
                         help="Path to username file (one username per line)")
     parser.add_argument("-p", "--pass-file", dest="pass_file", required=True,
@@ -70,14 +87,23 @@ def main(argv=None):
     args = parser.parse_args(argv)
 
     host = args.host
+    mode = args.mode
+    data = args.data
+
+    funcs = [httppostjson]
+    funcs_bad_request = [httppostjson_bad_request]
 
     # Validate URL
     if not is_valid_url(args.host):
         parser.error(f"Invalid host URL: {args.host}")
+    if mode not in mode_list:
+        parser.error(f"The mode must be in the following list : "+str(mode))
+    
+    mode_index = mode_list.index("http-post-json")
 
     print("------------")
     print("URL : "+host)
-    bad_request_data = bad_request(host)
+    bad_request_data = funcs_bad_request[mode_index](host,data)
     print("------------")
 
     user_path = Path(args.user_file)
@@ -96,7 +122,7 @@ def main(argv=None):
             counter += 1
             if(counter % progressvalue)==0:
                 print(f"\rProgress {counter}/{totalnumber}", end="")
-            send(host,user,password,bad_request_data)
+            funcs[mode_index](host,user,password,bad_request_data,data)
     print()
     global found 
     if not found:
